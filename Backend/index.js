1cef8f7ee2979fc6abd2f30c526a2e24367de814
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-03-23 23:32:20 +0500

    update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,29 @@
 import { ConnectMongoDb } from "./Config/db";
 import { configDotenv } from "dotenv";
 import express from express
+import passport from "passport";
+import session from "express-session";
+import GoogleStrategy from 'passport-google-oauth2'
 configDotenv()
 ConnectMongoDb()
 
 const app=express()
 
-app.get('/',(req,res)=>{
+app.use(session({
+  secret:'StudySync_Secret',
+  resave:false,
+  saveUninitialized:false
+}))
+app.get('/studysync',(req,res)=>{
     res.send('hello from the server')
+})
+
+app.get('/auth/google',(req,res)=>{
+    passport.authenticate('google',{scope:['email','profile']})
+})
+
+app.get('/auth/google/callback',(req,res)=>{
+    
 })
 
 
