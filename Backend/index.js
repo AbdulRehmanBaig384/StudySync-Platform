3d3e75baae319774bb5692cedfd00653f7ceb45e
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-03-23 19:53:07 +0500

    backend code update

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,14 @@ ConnectMongoDb()
 
 const app=express()
 
+app.get('/',(req,res)=>{
+    res.send('hello from the server')
+})
+
+
+app.listen((process.env.PORT),()=>{
+      console.log(`server is running at https://localhost/${process.env.PORT}`)
+})
 
 
 
