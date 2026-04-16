922fa282e2c5cf0a01ad0cbfad4fa7785ee98c4a
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-04-16 23:19:24 +0500

    Refactor import statements in BenefitsSection.jsx

--- Frontend/src/components/BenefitsSection.jsx
+++ Frontend/src/components/BenefitsSection.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import {HiOutlineLightBulb, HiOutlineChatAlt2,HiOutlineTrendingUp,HiOutlineBookOpen,
-  HiOutlineAcademicCap,
-  HiOutlineVideoCamera,
+import {HiOutlineLightBulb, HiOutlineChatAlt2,HiOutlineTrendingUp,HiOutlineBookOpen,HiOutlineAcademicCap,HiOutlineVideoCamera,
   HiOutlineGlobe,
   HiOutlineStar,
 } from 'react-icons/hi'
