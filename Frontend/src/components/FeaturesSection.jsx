36dec355f85749c3dbb11a35ca81beb3a58c9aa8
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-04-17 12:09:06 +0500

    Refactor import statement for react-icons

--- Frontend/src/components/FeaturesSection.jsx
+++ Frontend/src/components/FeaturesSection.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { HiOutlineUsers, HiOutlineVideoCamera,HiOutlineCode, HiOutlineClipboardCheck,
-  HiOutlineFolderOpen,
-} from 'react-icons/hi'
+import { HiOutlineUsers, HiOutlineVideoCamera,HiOutlineCode, HiOutlineClipboardCheck, HiOutlineFolderOpen,} from 'react-icons/hi'
 import { FiArrowRight } from 'react-icons/fi'
 
 const features = [
