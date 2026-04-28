578e23e176aa9bb21e3a1442c54b959ba141d531
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-04-29 02:02:26 +0500

    Update Invitation.js

--- Backend/models/Invitation.js
+++ Backend/models/Invitation.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 
 const invitationSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
+  sender:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required: true},
   receiver: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
