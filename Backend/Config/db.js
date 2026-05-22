a8f3136cff6f0365185ce6f72d179b1421d35080
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-22 10:25:44 +0500

    Update db.js

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const ConnectMongoDb = async () => {
-  try {
+  try{
     const connDB = await mongoose.connect(process.env.MONGO_DB);
 
     console.log("mogodb has been successfully conntected");
