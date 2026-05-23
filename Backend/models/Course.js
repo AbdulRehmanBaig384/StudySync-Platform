4e9eaebe80986dac831c7c5b8562ff6be9370f5d
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-05-23 14:42:54 +0500

    Update Course.js

--- Backend/models/Course.js
+++ Backend/models/Course.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const courseSchema = new mongoose.Schema({
   title:{type:String,required:true},
   category:{type:String,required:true },
-  description: { type: String },
-  image: { type: String }
-}, { timestamps: true });
+  description: {type:String },
+  image:{type:String }
+}, {timestamps: true });
 
 export default mongoose.model('Course', courseSchema);
